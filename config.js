05e9cec1e988a590ab062931d9f25d7f9fca9692
sanamcha <mhr.sanam@gmail.com>
2022-05-06 09:04:20 -0700

    modified config

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function getDatabaseUri() {
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-console.log("recipedb Config:".green);
+console.log("meals_app Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
